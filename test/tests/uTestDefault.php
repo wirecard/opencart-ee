8c9669a29af1be6031f403d04667e1c817213018
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-05-22 11:11:16 +0200

    #2 Update namespace

--- test/tests/uTestDefault.php
+++ test/tests/uTestDefault.php
@@ -29,7 +29,7 @@
  * Please do not use the plugin if you do not agree to these terms of use!
  */
 
-class uTestDefault extends \PHPUnit\Framework\TestCase
+class uTestDefault extends \PHPUnit_Framework_TestCase
 {
     public function testDefaultUtest()
     {
