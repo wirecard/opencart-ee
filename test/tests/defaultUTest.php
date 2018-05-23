5f5cb6e152f50266b95ca0c6e9d25eb13c089144
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-05-23 11:24:14 +0200

    #2 Set sufix for unitests

--- test/tests/defaultUTest.php
+++ test/tests/defaultUTest.php
@@ -29,7 +29,7 @@
  * Please do not use the plugin if you do not agree to these terms of use!
  */
 
-class uTestDefault extends \PHPUnit_Framework_TestCase
+class defaultUTest extends \PHPUnit_Framework_TestCase
 {
     public function testDefaultUtest()
     {
