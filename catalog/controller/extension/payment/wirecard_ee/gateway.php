aaf372354a83e081b2b80e142cbd5a21d2a461cc
Jacqueline Rinnhofer <jacqueline.rinnhofer@wirecard.com>
2018-05-30 15:15:31 +0200

    #5 Add basic frontend payment model

---
+++ catalog/controller/extension/payment/wirecard_ee/gateway.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
+
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . DIR_SYSTEM . '/library');
+
+require_once 'vendor/autoload.php';
+
+/**
+ * Class ControllerExtensionPaymentGateway
+ *
+ * Basic payment extension controller
+ *
+ * @since 1.0.0
+ */
+abstract class ControllerExtensionPaymentGateway extends Controller{
+
+    /**
+     * @var string
+     * @since 1.0.0
+     */
+    private $pluginVersion = '1.0.0';
+
+    /**
+     * @var string
+     * @since 1.0.0
+     */
+    protected $prefix = 'payment_wirecard_ee_';
+
+    /**
+     * @var string
+     * @since 1.0.0
+     */
+    protected $type;
+
+    public function index()
+    {
+        $prefix = $this->prefix . $this->type;
+
+        $this->load->model('checkout/order');
+
+        $this->load->language('extension/payment/wirecard_ee');
+        $this->load->language('extension/payment/wirecard_ee_' . $this->type);
+
+        $data['active'] = $this->config->get($this->prefix . $this->type . '_status');
+
+        return $this->load->view('extension/payment/wirecard_ee', $data);
+    }
+}
