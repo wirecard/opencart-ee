4a1f67ba3f5981f641869ab00bd512b08d7a64ad
Richard Blechinger <me@blechi.at>
2018-06-14 10:29:03 +0200

    #20 Disable order number setter

--- catalog/model/extension/payment/wirecard_pg/helper/additional_information_helper.php
+++ catalog/model/extension/payment/wirecard_pg/helper/additional_information_helper.php
@@ -120,7 +120,7 @@ class AdditionalInformationHelper extends Model {
 			if (strlen($order['customer_id'])) {
 				$transaction->setConsumerId($order['customer_id']);
 			}
-			$transaction->setOrderNumber($order['order_id']);
+			//$transaction->setOrderNumber($order['order_id']);
 			$transaction->setDescriptor($this->createDescriptor($order));
 
 			$accountHolder = new PGAccountHolder();
