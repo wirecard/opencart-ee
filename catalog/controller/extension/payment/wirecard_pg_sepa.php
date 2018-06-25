a0f67758aaa6010e6c0148a2b75b91da9d8a22c9
Richard Blechinger <me@blechi.at>
2018-06-25 13:41:29 +0200

    #43 Add SEPA payment method configuration

---
+++ catalog/controller/extension/payment/wirecard_pg_sepa.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
+ */
+
+require_once(dirname(__FILE__) . '/wirecard_pg/gateway.php');
+
+use Wirecard\PaymentSdk\Transaction\SepaTransaction;
+use Wirecard\PaymentSdk\Config\PaymentMethodConfig;
+
+/**
+ * Class ControllerExtensionPaymentWirecardPGSepa
+ *
+ * SEPA Transaction controller
+ *
+ * @since 1.0.0
+ */
+class ControllerExtensionPaymentWirecardPGSepa extends ControllerExtensionPaymentGateway {
+
+	/**
+	 * @var string
+	 * @since 1.0.0
+	 */
+	protected $type = 'sepa';
+
+	/**
+	 * Basic index method
+	 *
+	 * @since 1.0.0
+	 */
+	public function index($data = null) {
+		return parent::index();
+	}
+
+	/**
+	 * Create paypal transaction
+	 *
+	 * @since 1.0.0
+	 */
+	public function confirm() {
+
+		$this->transaction = new SepaTransaction();
+
+		parent::confirm();
+	}
+
+	/**
+	 * Create payment specific config
+	 *
+	 * @param array $currency
+	 * @return \Wirecard\PaymentSdk\Config\Config
+	 * @since 1.0.0
+	 */
+	public function getConfig($currency = null) {
+		$merchant_account_id = $this->getShopConfigVal('merchant_account_id');
+		$merchant_secret = $this->getShopConfigVal('merchant_secret');
+
+		$config = parent::getConfig($currency);
+		$paymentConfig = new PaymentMethodConfig(SepaTransaction::NAME, $merchant_account_id, $merchant_secret);
+		$config->add($paymentConfig);
+
+		return $config;
+	}
+
+	/**
+	 * Payment specific model getter
+	 *
+	 * @return Model
+	 * @since 1.0.0
+	 */
+	public function getModel() {
+		$this->load->model('extension/payment/wirecard_pg_' . $this->type);
+
+		return $this->model_extension_payment_wirecard_pg_paypal;
+	}
+
+	/**
+	 * Get new instance of payment specific transaction
+	 *
+	 * @return SepaTransaction
+	 * @since 1.0.0
+	 */
+	public function getTransactionInstance() {
+		return new SepaTransaction();
+	}
+
+	/**
+	 * Create SEPA cancel transaction
+	 *
+	 * @param array $parentTransaction
+	 * @return \Wirecard\PaymentSdk\Transaction\Transaction
+	 * @since 1.0.0
+	 */
+	public function createCancelTransaction($parentTransaction) {
+		$this->transaction = new SepaTransaction();
+
+		return parent::createCancelTransaction($parentTransaction);
+	}
+
+	/**
+	 * Create SEPA success transaction
+	 *
+	 * @param array $parentTransaction
+	 * @param float $captureAmount
+	 * @return \Wirecard\PaymentSdk\Transaction\Transaction
+	 * @since 1.0.0
+	 */
+	public function createCaptureTransaction($parentTransaction, $captureAmount = null) {
+		$this->transaction = new SepaTransaction();
+
+		return parent::createCaptureTransaction($parentTransaction, $captureAmount);
+	}
+}
+
