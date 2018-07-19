9199d5dbe9031c06ef10f50708922fab05565eb1
Richard Blechinger <me@blechi.at>
2018-07-19 12:55:06 +0200

    #93 Implement Masterpass payment method

---
+++ catalog/controller/extension/payment/wirecard_pg_masterpass.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/opencart-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/opencart-ee/blob/master/LICENSE
+ */
+
+require_once(dirname(__FILE__) . '/wirecard_pg/gateway.php');
+
+use Wirecard\PaymentSdk\Transaction\MasterpassTransaction;
+use Wirecard\PaymentSdk\Config\PaymentMethodConfig;
+
+/**
+ * Class ControllerExtensionPaymentWirecardPGMasterpass
+ *
+ * Masterpass Transaction controller
+ *
+ * @since 1.1.0
+ */
+class ControllerExtensionPaymentWirecardPGMasterpass extends ControllerExtensionPaymentGateway {
+
+	/**
+	 * @var string
+	 * @since 1.1.0
+	 */
+	protected $type = 'masterpass';
+
+	/**
+	 * Basic index method
+	 *
+	 * @since 1.1.0
+	 */
+	public function index($data = null) {
+		return parent::index();
+	}
+
+	/**
+	 * Create Masterpass transaction
+	 *
+	 * @since 1.1.0
+	 */
+	public function confirm() {
+		$this->load->model('checkout/order');
+		$this->transaction = $this->getTransactionInstance();
+
+		$order = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+		$additional_helper = new AdditionalInformationHelper($this->registry, $this->prefix . $this->type, $this->config);
+
+		$additional_helper->addAccountHolder($this->transaction, $order);
+
+		parent::confirm();
+	}
+
+	/**
+	 * Create payment specific config
+	 *
+	 * @param array $currency
+	 * @return \Wirecard\PaymentSdk\Config\Config
+	 * @since 1.1.0
+	 */
+	public function getConfig($currency = null) {
+		$merchant_account_id = $this->getShopConfigVal('merchant_account_id');
+		$merchant_secret = $this->getShopConfigVal('merchant_secret');
+
+		$config = parent::getConfig($currency);
+		$payment_config = new PaymentMethodConfig(MasterpassTransaction::NAME, $merchant_account_id, $merchant_secret);
+		$config->add($payment_config);
+
+		return $config;
+	}
+
+	/**
+	 * Payment specific model getter
+	 *
+	 * @return Model
+	 * @since 1.1.0
+	 */
+	public function getModel() {
+		$this->load->model('extension/payment/wirecard_pg_' . $this->type);
+
+		return $this->model_extension_payment_wirecard_pg_masterpass;
+	}
+
+	/**
+	 * Get new instance of payment specific transaction
+	 *
+	 * @return MasterpassTransaction
+	 * @since 1.1.0
+	 */
+	public function getTransactionInstance() {
+		return new MasterpassTransaction();
+	}
+
+	/**
+	 * Create Masterpass transaction
+	 *
+	 * @param array $parent_transaction
+	 * @param \Wirecard\PaymentSdk\Entity\Amount $amount
+	 * @param string $operation
+	 * @return \Wirecard\PaymentSdk\Transaction\Transaction
+	 * @since 1.1.0
+	 */
+	public function createTransaction($parent_transaction, $amount) {
+		$this->transaction = $this->getTransactionInstance();
+
+		return parent::createTransaction($parent_transaction, $amount);
+	}
+}
+
--- catalog/model/extension/payment/wirecard_pg/helper/additional_information_helper.php
+++ catalog/model/extension/payment/wirecard_pg/helper/additional_information_helper.php
@@ -1,15 +1,36 @@
 <?php
 /**
- * Shop System Plugins:
- * - Terms of Use can be found under:
- * https://github.com/wirecard/opencart-ee/blob/master/_TERMS_OF_USE
- * - License can be found under:
- * https://github.com/wirecard/opencart-ee/blob/master/LICENSE
+ * Shop System Plugins - Terms of Use
+ *
+ * The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+ * of the Wirecard AG range of products and services.
+ *
+ * They have been tested and approved for full functionality in the standard configuration
+ * (status on delivery) of the corresponding shop system. They are under General Public
+ * License version 3 (GPLv3) and can be used, developed and passed on to third parties under
+ * the same terms.
+ *
+ * However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+ * occurring when used in an enhanced, customized shop system configuration.
+ *
+ * Operation in an enhanced, customized configuration is at your own risk and requires a
+ * comprehensive test phase by the user of the plugin.
+ *
+ * Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+ * functionality neither does Wirecard AG assume liability for any disadvantages related to
+ * the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+ * for customized shop systems or installed plugins of other vendors of plugins within the same
+ * shop system.
+ *
+ * Customers are responsible for testing the plugin's functionality before starting productive
+ * operation.
+ *
+ * By installing the plugin into the shop system the customer agrees to these terms of use.
+ * Please do not use the plugin if you do not agree to these terms of use!
  */
 
 require_once(dirname(__FILE__) . '/pg_basket.php');
 require_once(dirname(__FILE__) . '/pg_account_holder.php');
-include_once(DIR_SYSTEM . 'library/autoload.php');
 
 use Wirecard\PaymentSdk\Transaction\Transaction;
 
@@ -19,7 +40,8 @@ use Wirecard\PaymentSdk\Transaction\Transaction;
  * @since 1.0.0
  */
 class AdditionalInformationHelper extends Model {
-
+	const OPENCART_GATEWAY_WIRECARD_VERSION = '1.0.0';
+	const OPENCART_GATEWAY_WIRECARD_NAME = 'Wirecard OpenCart Extension';
 	const CURRENCYCODE = 'currency_code';
 	const CURRENCYVALUE = 'currency_value';
 
@@ -64,6 +86,23 @@ class AdditionalInformationHelper extends Model {
 	}
 
 	/**
+	 * Add account holder to transaction.
+	 *
+	 * @param Transaction $transaction
+	 * @param $order
+	 * @return Transaction
+	 * @since 1.0.0
+	 */
+	public function addAccountHolder($transaction, $order) {
+		$account_holder = new PGAccountHolder();
+
+		$transaction->setAccountHolder($account_holder->createAccountHolder($order, $account_holder::BILLING));
+		$transaction->setShipping($account_holder->createAccountHolder($order, $account_holder::SHIPPING));
+
+		return $transaction;
+	}
+
+	/**
 	 * Create identification data
 	 *
 	 * @param Transaction $transaction
@@ -72,13 +111,12 @@ class AdditionalInformationHelper extends Model {
 	 * @since 1.0.0
 	 */
 	public function setIdentificationData($transaction, $order) {
-		$basic_info = new ExtensionModuleWirecardPGPluginData();
 		$custom_fields = new \Wirecard\PaymentSdk\Entity\CustomFieldCollection();
 		$custom_fields->add(new \Wirecard\PaymentSdk\Entity\CustomField('orderId', $order['order_id']));
 		$custom_fields->add(new \Wirecard\PaymentSdk\Entity\CustomField('shopName', 'OpenCart'));
 		$custom_fields->add(new \Wirecard\PaymentSdk\Entity\CustomField('shopVersion', VERSION));
-		$custom_fields->add(new \Wirecard\PaymentSdk\Entity\CustomField('pluginName', $basic_info->getName()));
-		$custom_fields->add(new \Wirecard\PaymentSdk\Entity\CustomField('pluginVersion', $basic_info->getVersion()));
+		$custom_fields->add(new \Wirecard\PaymentSdk\Entity\CustomField('pluginName', self::OPENCART_GATEWAY_WIRECARD_NAME));
+		$custom_fields->add(new \Wirecard\PaymentSdk\Entity\CustomField('pluginVersion', self::OPENCART_GATEWAY_WIRECARD_VERSION));
 		$transaction->setCustomFields($custom_fields);
 		$transaction->setLocale(substr($order['language_code'], 0, 2));
 
@@ -103,23 +141,19 @@ class AdditionalInformationHelper extends Model {
 			));
 		}
 
-			if ($order['ip']) {
-				$transaction->setIpAddress($order['ip']);
-			} else {
-				$transaction->setIpAddress($_SERVER['REMOTE_ADDR']);
-			}
+		if ($order['ip']) {
+			$transaction->setIpAddress($order['ip']);
+		} else {
+			$transaction->setIpAddress($_SERVER['REMOTE_ADDR']);
+		}
 
-			if (strlen($order['customer_id'])) {
-				$transaction->setConsumerId($order['customer_id']);
-			}
-			$transaction->setOrderNumber($order['order_id'] . '12312');
-			$transaction->setDescriptor($this->createDescriptor($order));
+		if (strlen($order['customer_id'])) {
+			$transaction->setConsumerId($order['customer_id']);
+		}
+		$transaction->setOrderNumber($order['order_id']);
+		$transaction->setDescriptor($this->createDescriptor($order));
 
-			$account_holder = new PGAccountHolder();
-			$transaction->setAccountHolder($account_holder->createAccountHolder($order, $account_holder::BILLING));
-			$transaction->setShipping($account_holder->createAccountHolder($order, $account_holder::SHIPPING));
-
-			return $transaction;
+		return $transaction;
 	}
 
 	/**
