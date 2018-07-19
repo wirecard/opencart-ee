0955f2722c652e97527ae6cd2e157c3c508b82c4
Jacqueline Rinnhofer <jacqueline.rinnhofer@wirecard.com>
2018-07-19 10:42:28 +0200

    Update deployment process

--- .bin/generate-tag.sh
+++ .bin/generate-tag.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
+BRANCH="master"
 
-VERSION=`cat VERSION`
-STATUS=`curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/wirecard/opencart-ee/git/refs/tags/${VERSION}`
+if [ "$TRAVIS_BRANCH" == "BRANCH" ]; then
+    if [ "$TRAVIS_PULL_REQUEST" == false ]; then
 
-if [[ ${STATUS} == "200" ]] ; then
-    echo "Tag is up to date with version."
-    exit 0
-elif [[ ${STATUS} != "404" ]] ; then
-    echo "Got status ${STATUS} from GitHub. Exiting."
-    exit 0
-else
-    echo "Version is updated, creating tag ${VERSION}"
+        VERSION=`cat VERSION`
+        STATUS=`curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/wirecard/opencart-ee/git/refs/tags/${VERSION}`
+
+        if [[ ${STATUS} == "200" ]] ; then
+            echo "Tag is up to date with version."
+            exit 0
+        elif [[ ${STATUS} != "404" ]] ; then
+            echo "Got status ${STATUS} from GitHub. Exiting."
+            exit 0
+        else
+            echo "Version is updated, creating tag ${VERSION}"
+        fi
+
+        git config --global user.name "Travis CI"
+        git config --global user.email "wirecard@travis-ci.org"
+
+        git tag -a ${VERSION} -m "Pre-release version"
+        git push origin --tags
+        git fetch origin
+    fi
 fi
-
-git config user.name "Travis CI"
-git config user.email "wirecard@travis-ci.org"
-
-git tag -a ${VERSION} -m "Pre-release version"
-git push origin --tags
