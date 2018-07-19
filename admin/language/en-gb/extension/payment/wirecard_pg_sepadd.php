bb96687333214934f658920d6e7402efa1f25d8a
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-07-19 08:20:15 +0200

    #91 Merge sepa config from 1119

---
+++ admin/language/en-gb/extension/payment/wirecard_pg_sepadd.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/opencart-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/opencart-ee/blob/master/LICENSE
+ */
+
+// Page Title
+$_['heading_title'] = 'Wirecard SEPA Direct Debit';
+$_['text_wirecard_pg_sepadd'] = '<img src="./view/image/wirecard_pg/sepa.png" width="100"/>';
+
+// Payment specific configuration
+$_['text_edit'] = 'Edit SEPA Direct Debit';
+$_['config_status_desc'] = 'Activate payment method SEPA Direct Debit to make it available for your consumers.';
+
+
+$_['text_sepa_config'] = 'SEPA Credentials';
+$_['config_creditor_id'] = 'Creditor ID';
+$_['config_creditor_id_desc'] = 'SEPA Direct Debit requires Creditor ID to create SEPA Direct Debit Mandate. You may apply for a Creditor ID at your financial institution.';
+$_['config_creditor_name'] = 'Creditor Name';
+$_['config_creditor_name_desc'] = 'SEPA Direct Debit requires a Creditor Name for the SEPA Direct Debit Mandate page.';
+$_['config_creditor_city'] = 'Creditor City';
+$_['config_creditor_city_desc'] = 'SEPA Direct Debit requires a Creditor City for the SEPA Direct Debit Mandate page.';
+$_['config_mandate_text'] = 'Additional Text';
+$_['config_mandate_text_desc'] = 'This text appears on the SEPA Direct Debit Mandate page at the end of the first paragraph.';
+$_['config_enable_bic'] = 'BIC enabled';
+$_['config_enable_bic_desc'] = 'If BIC is activated, the consumer must enter a BIC in checkout.';
