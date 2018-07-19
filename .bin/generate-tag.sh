65b7f1c23376271b77dfaee10cc9dd6ec1da419a
Jacqueline Rinnhofer <jacqueline.rinnhofer@wirecard.com>
2018-07-19 10:25:38 +0200

    Update generate-tag script

--- .bin/generate-tag.sh
+++ .bin/generate-tag.sh
@@ -1,21 +1,20 @@
-if [ -z "$TRAVIS_TAG" ]; then
-    VERSION=`cat VERSION`
-    STATUS=`curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/wirecard/opencart-ee/git/refs/tags/${VERSION}`
+#!/bin/bash
 
-    if [[ ${STATUS} == "200" ]] ; then
-        echo "Tag is up to date with version."
-        exit 0
-    elif [[ ${STATUS} != "404" ]] ; then
-        echo "Got status ${STATUS} from GitHub. Exiting."
-        exit 0
-    else
-        echo "Version is updated, creating tag ${VERSION}"
-    fi
+VERSION=`cat VERSION`
+STATUS=`curl -s -o /dev/null -w "%{http_code}" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/wirecard/opencart-ee/git/refs/tags/${VERSION}`
 
-    git config user.name "Travis CI"
-    git config user.email "wirecard@travis-ci.org"
-
-    git tag -a ${VERSION} -m "Pre-release version"
-    git push origin --tags
-
+if [[ ${STATUS} == "200" ]] ; then
+    echo "Tag is up to date with version."
+    exit 0
+elif [[ ${STATUS} != "404" ]] ; then
+    echo "Got status ${STATUS} from GitHub. Exiting."
+    exit 0
+else
+    echo "Version is updated, creating tag ${VERSION}"
 fi
+
+git config user.name "Travis CI"
+git config user.email "wirecard@travis-ci.org"
+
+git tag -a ${VERSION} -m "Pre-release version"
+git push origin --tags
