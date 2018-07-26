390d2ea6fa639af149b34b435d0a2079d6c2b5b7
Jacqueline Rinnhofer <jacqueline.rinnhofer@wirecard.com>
2018-07-26 12:33:40 +0200

    #104 Add invoice catalog model

---
+++ catalog/model/extension/payment/wirecard_pg_ratepayinvoice.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/opencart-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/opencart-ee/blob/master/LICENSE
+ */
+
+require_once(dirname( __FILE__ ) . '/wirecard_pg/gateway.php');
+
+/**
+ * Class ModelExtensionPaymentWirecardPGRatepayInvoice
+ *
+ * Guaranteed Invoice Transaction model
+ *
+ * @since 1.1.0
+ */
+class ModelExtensionPaymentWirecardPGRatepayInvoice extends ModelExtensionPaymentGateway {
+
+	/**
+	 * @var string
+	 * @since 1.1.0
+	 */
+	protected $type = 'ratepayinvoice';
+}
