96b19be3a23ff1d5d92fc0ba3d0194a4b0591ea0
Richard Blechinger <me@blechi.at>
2018-08-14 13:43:11 +0200

    Refactor FormInteractionResponse to be more future proof

--- catalog/model/extension/payment/wirecard_pg/gateway.php
+++ catalog/model/extension/payment/wirecard_pg/gateway.php
@@ -99,14 +99,17 @@ abstract class ModelExtensionPaymentGateway extends Model {
 		if ($response instanceof \Wirecard\PaymentSdk\Response\InteractionResponse) {
 			$redirect = $response->getRedirectUrl();
 		} elseif ($response instanceof \Wirecard\PaymentSdk\Response\FormInteractionResponse) {
-			if ('creditcard' == $this->type) {
-				return $this->handleFormInteractionResponse($response);
-			}
 			$form_fields = $response->getFormFields();
 			$response_query = array();
+
 			foreach ($form_fields->getIterator() as $key => $value) {
 				$response_query[$key] = $value;
 			}
+
+			if (!array_key_exists('sync_response', $response_query)) {
+				return $this->handleFormInteractionPostRequest($response);
+			}
+
 			$query = http_build_query($response_query);
 			$redirect = $response->getUrl() . '&' . $query;
 		} elseif ($response instanceof \Wirecard\PaymentSdk\Response\FailureResponse) {
@@ -228,12 +231,12 @@ abstract class ModelExtensionPaymentGateway extends Model {
 	}
 
 	/**
-	 * Handle the form interaction response in an appropriate way.
+	 * Handles the redirection of a customer via a submitted form.
 	 *
 	 * @param \Wirecard\PaymentSdk\Response\FormInteractionResponse $response
 	 * @return mixed
 	 */
-	public function handleFormInteractionResponse($response) {
+	public function handleFormInteractionPostRequest($response) {
 		$this->load->language('information/static');
 		$this->load->language('language/extension/wirecard_pg');
 
