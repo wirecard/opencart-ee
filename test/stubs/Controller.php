aa329c29fbca9f44226e9000d40cbc0042ec8f04
tomazpu <32056440+tomazpu@users.noreply.github.com>
2018-08-21 09:22:33 +0200

    #130 Update cs

--- test/stubs/Controller.php
+++ test/stubs/Controller.php
@@ -7,19 +7,19 @@ use Mockery as m;
  */
 abstract class Controller
 {
-    public $response;
-    protected $registry;
-    protected $config;
-    protected $load;
-    protected $session;
-    protected $url;
-    protected $language;
-    protected $cart;
+	public $response;
+	protected $registry;
+	protected $config;
+	protected $load;
+	protected $session;
+	protected $url;
+	protected $language;
+	protected $cart;
 	protected $currency;
 	protected $transaction;
 	public $request;
 	public $model_extension_payment_wirecard_pg_paypal;
-    public $model_extension_payment_wirecard_pg_creditcard;
+	public $model_extension_payment_wirecard_pg_creditcard;
 	public $model_extension_payment_wirecard_pg_sepact;
 	public $model_extension_payment_wirecard_pg_sofortbanking;
 	public $model_extension_payment_wirecard_pg_ideal;
@@ -30,20 +30,20 @@ abstract class Controller
 	public $model_extension_payment_wirecard_pg_upi;
 	public $model_extension_payment_wirecard_pg_sepadd;
 	public $model_extension_payment_wirecard_pg_ratepayinvoice;
-    public $model_checkout_order;
-    public $controller_extension_payment_wirecard_pg_sepact;
+	public $model_checkout_order;
+	public $controller_extension_payment_wirecard_pg_sepact;
 
     public function __construct($registry, $config, $loader, $session, $response, $orderModel, $url, $modelPayment, $language, $cart, $currency, $subController = null, $document = null, $customer = null, $overrideRequest = null, $transaction = null)
     {
-        $this->registry = $registry;
-        $this->config = $config;
-        $this->load = $loader;
-        $this->session = $session;
-        $this->response = $response;
-        $this->model_checkout_order = $orderModel;
-        $this->url = $url;
-        $this->model_extension_payment_wirecard_pg_paypal = $modelPayment;
-        $this->model_extension_payment_wirecard_pg_creditcard = $modelPayment;
+		$this->registry = $registry;
+		$this->config = $config;
+		$this->load = $loader;
+		$this->session = $session;
+		$this->response = $response;
+		$this->model_checkout_order = $orderModel;
+		$this->url = $url;
+		$this->model_extension_payment_wirecard_pg_paypal = $modelPayment;
+		$this->model_extension_payment_wirecard_pg_creditcard = $modelPayment;
 		$this->model_extension_payment_wirecard_pg_sepact = $modelPayment;
 		$this->model_extension_payment_wirecard_pg_sofortbanking = $modelPayment;
 		$this->model_extension_payment_wirecard_pg_ideal = $modelPayment;
@@ -54,32 +54,32 @@ abstract class Controller
 		$this->model_extension_payment_wirecard_pg_pia = $modelPayment;
 		$this->model_extension_payment_wirecard_pg_sepadd = $modelPayment;
 		$this->model_extension_payment_wirecard_pg_ratepayinvoice = $modelPayment;
-        $this->language = $language;
-        $this->cart = $cart;
-        $this->controller_extension_payment_wirecard_pg_sepact = $subController;
-        $this->document = $document;
-        $this->customer = $customer;
-        $this->transaction = $transaction;
+		$this->language = $language;
+		$this->cart = $cart;
+		$this->controller_extension_payment_wirecard_pg_sepact = $subController;
+		$this->document = $document;
+		$this->customer = $customer;
+		$this->transaction = $transaction;
 
-	    $this->request = new stdClass();
-	    $this->request->post = [
-	    	'fingerprint-session' => '123',
+		$this->request = new stdClass();
+		$this->request->post = $overrideRequest ?: [
+			'fingerprint-session' => '123',
 			'ideal-bic' => \Wirecard\PaymentSdk\Entity\IdealBic::INGBNL2A
 		];
 
-	    $this->currency = $currency;
+		$this->currency = $currency;
 
-	    $this->model_extension_payment_wirecard_pg_vault = m::mock('overload:ModelExtensionPaymentWirecardPGVault');
+		$this->model_extension_payment_wirecard_pg_vault = m::mock('overload:ModelExtensionPaymentWirecardPGVault');
 		$this->model_extension_payment_wirecard_pg_vault->shouldReceive('getCards');
     }
 
-    public function get($key)
-    {
-        return $this->registry->get($key);
-    }
+	public function get($key)
+	{
+		return $this->registry->get($key);
+	}
 
-    public function set($key, $value)
-    {
-        $this->registry->set($key, $value);
-    }
+	public function set($key, $value)
+	{
+		$this->registry->set($key, $value);
+	}
 }
