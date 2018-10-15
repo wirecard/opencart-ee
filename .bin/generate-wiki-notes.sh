830a6fd477f5b0ab5f8b3676e9f80b628bc8dbb7
Richard Blechinger <me@blechi.at>
2018-10-15 14:49:37 +0200

    Make wiki update script be generic in terms of repository name

--- .bin/generate-wiki-notes.sh
+++ .bin/generate-wiki-notes.sh
@@ -6,7 +6,7 @@ git config --global user.email "wirecard@travis-ci.org"
 git clone https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.wiki
 composer make-wiki-notes
 
-cd opencart-ee.wiki
+cd $REPO_NAME
 git add Home.md
 git commit -m "BOT: Update shop plugin versions"
 git push https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.wiki
