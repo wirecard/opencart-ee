812b8f944697653bb1fbc41a004ac18058b7cdb7
Jacqueline Rinnhofer <jacqueline.rinnhofer@wirecard.com>
2018-10-16 11:26:54 +0200

    Use phraseapp executable from github

--- .bin/generate-language-files.sh
+++ .bin/generate-language-files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget http://github.com/phrase/phraseapp-client/releases/download/1.11.1/phraseapp_linux_386
-phraseapp_linux_386 projects list
+./phraseapp_linux_386 projects list
--- admin/language/en-gb/extension/module/wirecard_pg.php
+++ admin/language/en-gb/extension/module/wirecard_pg.php
@@ -1,57 +1,45 @@
 <?php
-/**
- * Shop System Plugins:
- * - Terms of Use can be found under:
- * https://github.com/wirecard/opencart-ee/blob/master/_TERMS_OF_USE
- * - License can be found under:
- * https://github.com/wirecard/opencart-ee/blob/master/LICENSE
- */
 
-// Page Title
-$_['heading_title'] = 'Wirecard';
-
-//Response table translations
-$_['credit_card_title'] = 'Credit Card';
-$_['maskedPan'] = 'Masked Pan';
-$_['token'] = 'Token';
-
-$_['basket_title'] = 'Basket';
-$_['name'] = 'Name';
-$_['quantity'] = 'Quantity';
-$_['amount'] = 'Amount';
-$_['description'] = 'Description';
-$_['article-number'] = 'Article Number';
-
-$_['shipping_title'] = 'Shipping';
-$_['account_holder_title'] = 'Account Owner';
-$_['last-name'] = 'Last Name';
-$_['first-name'] = 'First Name';
-$_['email'] = 'Email';
-$_['date-of-birth'] = 'Shipping';
-$_['phone'] = 'Phone';
-$_['street1'] = 'Street';
-$_['street2'] = 'Street 2';
-$_['city'] = 'City';
-$_['country'] = 'Country';
-$_['postal-code'] = 'Postal Code';
-$_['house-extension'] = 'House Extension';
-$_['merchant-crm-id'] = 'Merchant CRM ID';
-$_['gender'] = 'Gender';
-$_['social-security-number'] = 'Social Security Number';
-$_['shipping-method'] = 'Shipping Method';
-
-$_['transaction_details_title'] = 'Transaction Details';
-$_['maid'] = 'MAID';
-$_['transactionID'] = 'Transaction ID';
-$_['requestId'] = 'Request ID';
-$_['transactionType'] = 'Transaction Type';
-$_['transactionState'] = 'Transaction State';
-$_['requestedAmount'] = 'Amount';
-$_['descriptor'] = 'Descriptor';
-
-$_['payment_details_title'] = 'Details';
-$_['paymentMethod'] = 'Payment Method';
-$_['timeStamp'] = 'Date';
-$_['customerId'] = 'Customer ID';
-$_['ip'] = 'IP Address';
-$_['orderNumber'] = 'Order Number';
+$lang['account_holder_title'] = 'Account Owner';
+$lang['amount'] = 'Amount';
+$lang['article-number'] = 'Article Number';
+$lang['basket_title'] = 'Basket';
+$lang['city'] = 'City';
+$lang['country'] = 'Country';
+$lang['credit_card_title'] = 'Credit Card';
+$lang['customerId'] = 'Customer ID';
+$lang['date-of-birth'] = 'Shipping';
+$lang['description'] = 'Description';
+$lang['descriptor'] = 'Descriptor';
+$lang['email'] = 'Email';
+$lang['first-name'] = 'First Name';
+$lang['gender'] = 'Gender';
+$lang['heading_title'] = 'Wirecard';
+$lang['house-extension'] = 'House Extension';
+$lang['ip'] = 'IP Address';
+$lang['last-name'] = 'Last Name';
+$lang['maid'] = 'MAID';
+$lang['maskedPan'] = 'Masked Pan';
+$lang['merchant-crm-id'] = 'Merchant CRM ID';
+$lang['name'] = 'Name';
+$lang['next_test_key'] = 'This is another test key with other stuff';
+$lang['orderNumber'] = 'Order Number';
+$lang['paymentMethod'] = 'Payment Method';
+$lang['payment_details_title'] = 'Details';
+$lang['phone'] = 'Phone';
+$lang['postal-code'] = 'Postal Code';
+$lang['quantity'] = 'Quantity';
+$lang['requestedAmount'] = 'Amount';
+$lang['requestId'] = 'Request ID';
+$lang['shipping-method'] = 'Shipping Method';
+$lang['shipping_title'] = 'Shipping';
+$lang['social-security-number'] = 'Social Security Number';
+$lang['street1'] = 'Street';
+$lang['street2'] = 'Street 2';
+$lang['test_tags'] = 'check for new tags and test it here!';
+$lang['timeStamp'] = 'Date';
+$lang['token'] = 'Token';
+$lang['transactionID'] = 'Transaction ID';
+$lang['transactionState'] = 'Transaction State';
+$lang['transactionType'] = 'Transaction Type';
+$lang['transaction_details_title'] = 'Transaction Details';
