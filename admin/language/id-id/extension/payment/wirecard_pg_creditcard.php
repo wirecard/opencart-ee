2991297370237dc721fa3ba365462f0e9cb58678
Jacqueline Rinnhofer <jacqueline.rinnhofer@wirecard.com>
2018-11-20 14:16:59 +0100

    #153 Add admin creditcard payment translations for apac

---
+++ admin/language/id-id/extension/payment/wirecard_pg_creditcard.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['config_allow_changed_shipping'] = 'Izinkan Perubahan Alamat Pengiriman';
+$lang['config_allow_changed_shipping_desc'] = 'Jika dinonaktifkan, pelanggan harus memasukkan detail kartu kredit jika alamat pengiriman telah diubah di antara dua pesanan.';
+$lang['config_limit_desc'] = 'Jumlah dalam mata uang belanja default';
+$lang['config_merchant_account_id_cc_desc'] = 'Pengidentifikasi unik diterapkan pada akun penjual Anda. Dapat diatur menjadi “null” untuk menjalankan proses 3-D.';
+$lang['config_merchant_secret_cc_desc'] = 'Secret Key penting untuk mengkalkulasi Tanda Tangan Digital untuk pembayaran. Dapat diatur menjadi “null” untuk menjalankan proses 3-D.';
+$lang['config_ssl_max_limit'] = 'Batas Maksimum untuk Non 3-D Secure';
+$lang['config_status_desc'] = 'Aktifkan metode pembayaran Kartu Kredit untuk menyediakannya kepada pelanggan Anda.';
+$lang['config_three_d_merchant_account_id'] = 'MAID 3-D Secure';
+$lang['config_three_d_merchant_account_id_desc'] = 'Pengidentifikasi unik diterapkan pada akun 3-D Secure Penjual Anda. Dapat diatur menjadi “null” untuk menjalankan proses SSL.';
+$lang['config_three_d_merchant_secret'] = 'Secret Key 3-D Secure';
+$lang['config_three_d_merchant_secret_desc'] = 'Secret Key penting untuk mengalkulasi Tanda Tangan Digital untuk pembayaran 3-D Secure. Dapat diatur menjadi “null” untuk menjalankan proses SSL.';
+$lang['config_three_d_min_limit'] = 'Batas Minimum untuk 3-D Secure';
+$lang['config_vault'] = 'Checkout Satu Klik';
+$lang['config_vault_desc'] = 'Detail Kartu Kredit disimpan untuk digunakan di kemudian hari.';
+$lang['heading_title'] = 'Kartu Kredit Wirecard';
+$lang['text_edit'] = 'Edit Kartu Kredit';
+$lang['text_vault'] = 'Checkout Satu Klik';
+$lang['text_wirecard_pg_creditcard'] = '<img src="./view/image/wirecard_pg/creditcard.png" />';
---
+++ admin/language/ja-jp/extension/payment/wirecard_pg_creditcard.php
@@ -0,0 +1,20 @@
+<?php
+
+$lang['config_allow_changed_shipping'] = '配送先住所の変更を許可する';
+$lang['config_allow_changed_shipping_desc'] = '無効の場合、購入者は2回の注文の間に配送先住所が変わったら、クレジットカードの詳細を再入力する必要があります。';
+$lang['config_limit_desc'] = '規定のショップ通貨の金額';
+$lang['config_merchant_account_id_cc_desc'] = 'マーチャント口座に割り当てられる一意の識別子です。「null」に設定して3-D処理を強制できます。';
+$lang['config_merchant_secret_cc_desc'] = 'Secret Key は、支払いのデジタル署名を計算するために必須です。「null」に設定して3-D処理を強制できます。';
+$lang['config_ssl_max_limit'] = '3-Dセキュア以外の最大限';
+$lang['config_status_desc'] = '購入者が使用できるように支払方法のクレジットカードを有効にします。';
+$lang['config_three_d_merchant_account_id'] = '3-Dセキュア MAID';
+$lang['config_three_d_merchant_account_id_desc'] = '3-Dセキュアマーチャント口座に割り当てられる一意の識別子です。「null」に設定してSSL処理を強制できます。';
+$lang['config_three_d_merchant_secret'] = '3-Dセキュア Secret Key';
+$lang['config_three_d_merchant_secret_desc'] = 'Secret Key は、3-Dセキュア支払いのデジタル署名を計算するために必須です。「null」に設定してSSL処理を強制できます。';
+$lang['config_three_d_min_limit'] = '3-Dセキュアの最小限';
+$lang['config_vault'] = 'ワンクリック注文';
+$lang['config_vault_desc'] = 'クレジットカードの詳細は後で使用するため保存されます。';
+$lang['heading_title'] = 'Wirecardクレジットカード';
+$lang['text_edit'] = 'クレジットカードの編集';
+$lang['text_vault'] = 'ワンクリック注文';
+$lang['text_wirecard_pg_creditcard'] = '<img src="./view/image/wirecard_pg/creditcard.png" />';
