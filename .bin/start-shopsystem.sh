0695cdb42dc87f87c6c88ef73dab12e2e3ecebf9
brkicadis <adis.brkic@wirecard.com>
2019-05-27 11:17:25 +0200

    Change travis shopsystem version extraction

--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -3,16 +3,7 @@
 export OPENCART_CONTAINER_NAME=opencart
 export OPENCART_CONTAINER_VERSION=${OPENCART_VERSION}
 
-docker volume create --name opencart_data
-docker run -d --name opencart -p 80:80 -p 443:443 \
-  -e MARIADB_HOST=mariadb \
-  -e MARIADB_PORT_NUMBER=3306 \
-  -e OPENCART_DATABASE_USER=bn_opencart \
-  -e OPENCART_DATABASE_NAME=bitnami_opencart \
-  --net opencart-tier \
-  --volume /path/to/opencart-persistence:/bitnami \
-  bitnami/opencart:${PRESTASHOP_CONTAINER_VERSION}
-#docker-compose up -d
+docker-compose up -d
 
 # wait for shop system to initialize
 while ! $(curl --silent --output /dev/null --head --fail "${OPENCART_URL}/admin"); do
