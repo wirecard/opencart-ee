47824b6bb1ddb4d110533970b8379e3b896ae86d
tatjana.starcenko <tatjana.starcenko@wirecard.com>
2019-05-31 10:07:05 +0200

    Change browserstack to ngrok

---
+++ .bin/start-ngrok.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# download and install ngrok
+curl -s https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > ngrok.zip
+unzip ngrok.zip
+chmod +x $PWD/ngrok
+# Download json parser for determining ngrok tunnel
+curl -sO http://stedolan.github.io/jq/download/linux64/jq
+chmod +x $PWD/jq
+
+# Open ngrok tunnel
+$PWD/ngrok authtoken $NGROK_TOKEN
+TIMESTAMP=$(date +%s)
+#$PWD/ngrok http 80 -subdomain=${TIMESTAMP}${GATEWAY}> /dev/null &
+$PWD/ngrok http 80 -subdomain="${OPENCART_DOMAIN}" > /dev/null &
+NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+
+# allow ngrok to initialize
+while [ ! ${NGROK_URL} ] || [ ${NGROK_URL} = 'null' ];  do
+    echo "Waiting for ngrok to initialize"
+    export NGROK_URL=$(curl -s localhost:4040/api/tunnels/command_line | jq --raw-output .public_url)
+    sleep 1
+done
--- .bin/start-shopsystem.sh
+++ .bin/start-shopsystem.sh
@@ -4,6 +4,7 @@ export OPENCART_CONTAINER_NAME=opencart
 
 docker-compose up -d
 
+echo "${OPENCART_URL}/admin"
 # wait for shop system to initialize
 while ! $(curl --silent --output /dev/null --head --fail "${OPENCART_URL}/admin"); do
     echo "Waiting for docker container to initialize"
