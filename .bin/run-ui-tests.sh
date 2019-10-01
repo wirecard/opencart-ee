66ea4e532b9fdce407679c3ba241da838b4569d6
brkicadis <adis.brkic@wirecard.com>
2019-10-01 08:24:52 +0200

    Spin up the shop system using mariadb password as an argument

--- .bin/run-ui-tests.sh
+++ .bin/run-ui-tests.sh
@@ -2,4 +2,4 @@ set -a # automatically export all variables from .env file
 source .env
 set +a
 
-system/library/bin/codecept run acceptance -g ui_test --html --xml
+system/library/bin/codecept run acceptance -g ui_test --steps -vvv
