c15ac43521454ea8f5f70a65cd82dd7ba6013345
brkicadis <adis.brkic@wirecard.com>
2019-10-01 11:27:54 +0200

    Try to execute script in docker context

--- .bin/install-extension-ui.sh
+++ .bin/install-extension-ui.sh
@@ -7,4 +7,4 @@ set +a
 # run installation from UI (missing part that cannot be done via command line)
 sleep 5
 
-system/library/bin/codecept run acceptance -g installator --steps
+docker exec --env GATEWAY=${GATEWAY} ${OPENCART_CONTAINER_NAME} ./plugin/system/library/bin/codecept run acceptance -g installator --steps
